48aabd41dd47d421d0e8a0a80f27c638f6272ad1
AMEUR Ramzi <double.ramzi@hotmail.com>
2018-02-20 22:03:26 -0500

    Add resources folder and css file

---
+++ src/main/java/pigeonssquare/Main.java
@@ -0,0 +1,27 @@
+package main.java.pigeonssquare;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/main.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("../../resources/main.css").toExternalForm());
+        primaryStage.setTitle("Pigeons Square");
+        primaryStage.setMaximized(true);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
